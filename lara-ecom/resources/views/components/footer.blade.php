b9d4f13bfab3abccaacac8591c1d930b210bbec7
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-20 02:41:01 +1000

    Feature: Footer

--- lara-ecom/resources/views/components/footer.blade.php
+++ lara-ecom/resources/views/components/footer.blade.php
@@ -1,6 +1,16 @@
-<footer class="w-full flex justify-center items-center p-3 border-t-2 border-gray-200 mt-5">
-	Made by
-	<?php echo date('Y'); ?>
-	Azaber
+<footer class="w-full flex flex-col justify-center items-center p-3 border-t-2 border-gray-200 mt-5">
+	<a href="https://azaber.com/" target="_blank">Made by
+		<?php echo date('Y'); ?>
+		Azaber</a>
+
+	@guest
+
+		<div class="flex justify-center">
+			<a href="{{ route('login') }}" class="text-gray-700 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Login</a>
+			<a href="{{ route('register') }}" class="text-gray-700 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Register</a>
+		</div>
+
+	@endguest
+
 </footer>
 
