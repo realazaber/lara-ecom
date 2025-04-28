914c85fcf597cfdf296b666183cf964d81ab9262
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 01:44:43 +1000

    UI: footer login links to the side instead of under

--- lara-ecom/resources/views/components/footer.blade.php
+++ lara-ecom/resources/views/components/footer.blade.php
@@ -1,13 +1,13 @@
-<footer class="w-full flex flex-col justify-center items-center p-3 border-t-2 border-gray-200 mt-5">
+<footer class="w-full flex flex-col justify-center items-center p-3 border-t-2 border-gray-200 mt-5 ">
 	<a href="https://azaber.com/" target="_blank">Made by
 		<?php echo date('Y'); ?>
 		Azaber</a>
 
 	@guest
 
-		<div class="flex justify-center">
-			<a href="{{ route('login') }}" class="text-gray-700 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Login</a>
-			<a href="{{ route('register') }}" class="text-gray-700 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Register</a>
+		<div class="absolute right-0  flex justify-center">
+			<a href="{{ route('login') }}" class="text-gray-400 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium">Login</a>
+			<a href="{{ route('register') }}" class="text-gray-400 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium">Register</a>
 		</div>
 
 	@endguest
