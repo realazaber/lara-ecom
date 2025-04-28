405787b8bcd431799dbbcfacb9f953912fed0050
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 02:02:38 +1000

    Base Routes

--- lara-ecom/routes/web.php
+++ lara-ecom/routes/web.php
@@ -1,23 +1,46 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect('/home');
 });
 
-Route::get('/dashboard', function () {
-    $bestAnimal = 'Dog';
-    $data = ['Item 1', 'Item 2', 'Item 3'];
-    return view('dashboard', compact('data', 'bestAnimal'));
-})->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::resource('home', HomeController::class);
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('pages.dashboard');
+    })->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('products', ProductsController::class)->names([
+        'index'   => 'products.index',
+        'create'  => 'products.create',
+        'store'   => 'products.store',
+        'show'    => 'products.show',
+        'edit'    => 'products.edit',
+        'update'  => 'products.update',
+        'destroy' => 'products.destroy',
+    ]);
+
+    Route::resource('orders', OrdersController::class)->names([
+        'index'   => 'orders.index',
+        'create'  => 'orders.create',
+        'store'   => 'orders.store',
+        'show'    => 'orders.show',
+        'edit'    => 'orders.edit',
+        'update'  => 'orders.update',
+        'destroy' => 'orders.destroy',
+    ]);
 });
 
 require __DIR__ . '/auth.php';
