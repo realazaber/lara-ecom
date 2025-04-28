072797a223c45c36093c3e6d51df80b8b77b1a89
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 02:04:51 +1000

    Base Orders and Products pages

---
+++ lara-ecom/resources/views/pages/orders/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+	<x-slot name="header">Orders</x-slot>
+
+	<x-slot name="title">Orders</x-slot>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, explicabo quod numquam blanditiis commodi provident aspernatur velit, omnis dicta vel ullam ipsam fugit harum sint debitis, quisquam asperiores nobis facilis!</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, explicabo quod numquam blanditiis commodi provident aspernatur velit, omnis dicta vel ullam ipsam fugit harum sint debitis, quisquam asperiores nobis facilis!</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, explicabo quod numquam blanditiis commodi provident aspernatur velit, omnis dicta vel ullam ipsam fugit harum sint debitis, quisquam asperiores nobis facilis!</p>
+</x-app-layout>
+
---
+++ lara-ecom/resources/views/pages/products/index.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+	<x-slot name="title">Products</x-slot>
+	<x-slot name="header">Products</x-slot>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus, alias earum numquam suscipit quod, modi corrupti iure esse dolorum quasi ad officiis. Molestias at delectus vero laboriosam impedit eveniet!</p>
+</x-app-layout>
+
