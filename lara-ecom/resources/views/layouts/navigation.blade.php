dc92d7a009b4425dab10c43a1e73cc0d424811fc
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 02:03:04 +1000

    UI: Add links to Orders and Products page.

--- lara-ecom/resources/views/layouts/navigation.blade.php
+++ lara-ecom/resources/views/layouts/navigation.blade.php
@@ -12,8 +12,15 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->is('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('products.index')" :active="request()->is('products')">
+                        
+                        {{ __('Products') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('orders.index')" :active="request()->is('orders')">                        
+                        {{ __('Orders') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -67,8 +74,15 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->is('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('products.index')" :active="request()->is('products')">
+                        
+                Products
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('orders.index')" :active="request()->is('orders')">                        
+                Orders
             </x-responsive-nav-link>
         </div>
 
